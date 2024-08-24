903b0a4c843bfee178f06d58d815409f0d502981
C-hoc-code <cuogncfvtc.vn@gmail.com>
2024-08-24 19:37:26 +0700

    first commit

---
+++ DemoMVC/Controllers/StudentController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+namespace DemoMVC.Controllers;
+
+public class StudentController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+}
